6645c6b0e1ac25374975d196393e26958ce489f9
etukifiok65 <mretukxd@gmail.com>
2026-02-15 01:05:08 +0100

    Update base path in vite.config.ts to '/admin-dashboard'

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: '/Admin-Dashboard/', 
+  base: '/admin-dashboard', 
   
   plugins: [react()],
   resolve: {
